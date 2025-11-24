e57126e7ac67c8917200d37619dc7ada08ad45d0
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-24 18:57:36 -0500

    feat(types): add barrel file for type exports
    - Create index.ts for convenient imports
    - Re-export all types from Link, Collection, Tag, User
    - Export constants (colors, icons, defaults)
    - Enable single-line imports: import { Link, Tag } from '@/types'

---
+++ src/types/index.ts
@@ -0,0 +1,53 @@
+/**
+ * Type Definitions Barrel File
+ *
+ * Re-exports all type definitions from the types directory
+ * for convenient imports throughout the application.
+ *
+ * @example
+ * import { Link, Collection, Tag, User } from '@/types';
+ */
+
+// Link types
+export type {
+  Link,
+  CreateLinkInput,
+  UpdateLinkInput,
+  LinkMetadata,
+  LinkFilters,
+  LinkSortField,
+  SortOrder,
+  LinkSortOptions,
+} from "./link";
+
+// Collection types
+export type {
+  Collection,
+  CreateCollectionInput,
+  UpdateCollectionInput,
+  CollectionWithCount,
+} from "./collection";
+export { COLLECTION_COLORS, COLLECTION_ICONS } from "./collection";
+
+// Tag types
+export type {
+  Tag,
+  CreateTagInput,
+  UpdateTagInput,
+  TagWithCount,
+  TagSuggestion,
+} from "./tag";
+export { TAG_COLORS } from "./tag";
+
+// User types
+export type {
+  User,
+  UserPreferences,
+  ThemePreference,
+  ViewMode,
+  AuthState,
+  SignUpInput,
+  SignInInput,
+} from "./user";
+export { DEFAULT_USER_PREFERENCES } from "./user";
+
