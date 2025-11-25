5c800f54841cc3e405faea792acefb9edae555eb
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-25 09:56:49 -0500

    test(setup): add polyfills for react-router v7 compatibility
    Update Jest setup file with:
    
    - TextEncoder/TextDecoder polyfills from Node.js util module
      Required by react-router v7 which uses these Web APIs
    - window.matchMedia mock for components using media queries
    
    These polyfills ensure the test environment matches browser
    capabilities that react-router v7 expects to be available.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,35 @@
+/**
+ * Jest Setup File
+ *
+ * Configures the testing environment with necessary polyfills and setup.
+ */
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Polyfill TextEncoder/TextDecoder for Node.js test environment
+// Required by react-router v7
+import { TextEncoder, TextDecoder } from "util";
+
+Object.assign(global, {
+  TextEncoder,
+  TextDecoder,
+});
+
+// Mock window.matchMedia for components that use media queries
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
