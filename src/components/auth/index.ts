f9c96796a584d374a665a5fa3ad58542fea8d26a
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-25 09:55:36 -0500

    feat(auth): add auth components barrel file for clean exports
    Create index.ts barrel file that re-exports all authentication
    components for convenient importing:
    
    - LoginForm
    - SignupForm
    - ConfirmSignup
    - ForgotPassword
    
    This allows importing multiple auth components in a single line:
    import { LoginForm, SignupForm } from 'components/auth'

---
+++ src/components/auth/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Auth Components Barrel File
+ *
+ * Re-exports all authentication components for convenient imports.
+ */
+
+export { LoginForm } from "./LoginForm";
+export { SignupForm } from "./SignupForm";
+export { ConfirmSignup } from "./ConfirmSignup";
+export { ForgotPassword } from "./ForgotPassword";
+
