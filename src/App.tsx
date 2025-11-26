a82e6b27344615ee8ed1324c1d4a387037414162
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-26 11:40:28 -0500

    Wire App component to centralized routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@
  */
 
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
-import { ProtectedRoute } from './components/layout';
-import { AuthPage } from './pages/Auth';
-import { DashboardPage } from './pages/Dashboard';
-import { ROUTES } from './utils/constants';
+import { AppRoutes } from './routes';
 import './App.css';
 
 /**
@@ -24,29 +21,7 @@ function App() {
     <ThemeProvider defaultTheme="dark">
       <BrowserRouter>
         <AuthProvider>
-          <Routes>
-            {/* Public Routes */}
-            <Route path={ROUTES.LOGIN} element={<AuthPage />} />
-            <Route path={ROUTES.SIGNUP} element={<AuthPage />} />
-            <Route path={ROUTES.FORGOT_PASSWORD} element={<AuthPage />} />
-            <Route path={ROUTES.CONFIRM_SIGNUP} element={<AuthPage />} />
-
-            {/* Protected Routes */}
-            <Route
-              path={ROUTES.DASHBOARD}
-              element={
-                <ProtectedRoute>
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Default redirect */}
-            <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.DASHBOARD} replace />} />
-
-            {/* Catch all - redirect to dashboard */}
-            <Route path="*" element={<Navigate to={ROUTES.DASHBOARD} replace />} />
-          </Routes>
+          <AppRoutes />
         </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
